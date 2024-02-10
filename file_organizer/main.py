c95efab3df6bd873950d3ca95a32854d02d06ed7
Nyambevos <ruslan.bilokoniuk@gmail.com>
2024-02-10 16:28:27 +0200

    Code refactoring

--- file_organizer/main.py
+++ file_organizer/main.py
@@ -1,8 +1,9 @@
 
+import shutil
 import logging
 from pathlib import Path
 from threading import Thread
-import shutil
+from time import time
 
 
 class FileOrganizer:
@@ -55,22 +56,27 @@ class FileOrganizer:
         ord('Я'): 'Ya',     ord('я'): 'ya'
         }
 
-
     def __init__(self, start_folder) -> None:
         self.start_folder = start_folder
         self.threads = []
 
+    def start(self) -> None:
+        logging.debug('Start sorting')
+        start_time = time()
 
-    def start(self):
-        
-        logging.debug(f'Start sorting')
-        self._r_sort(self.start_folder)
+        self._scan_folder(self.start_folder)
         [el.join() for el in self.threads]
+
         self._clean_folder()
-        logging.debug(f'Finish sorted')
 
+        finish_time = time()
+        logging.debug(f'Finish sorted. Time: {finish_time-start_time}')
 
-    def _r_sort(self, path_folder):
+    def _scan_folder(self, path_folder: Path) -> None:
+        """The function goes through the target folder.
+        The found subfolders are recursively opened
+        in a new folder for scanning. The found files
+        are processed and moved to a new thread."""
         logging.debug(f'scanning folder: {path_folder.name}')
 
         thread: Thread
@@ -79,98 +85,132 @@ class FileOrganizer:
             # Skip hidden files or folders (those starting with ".")
             if path.name[0] == ".":
                 continue
-    
+
             # Check if the current entry is a directory
             if path.is_dir():
                 if (path.parent == self.start_folder and
-                    path.name in self.TYPE_FILE):
+                        path.name in self.TYPE_FILE):
                     continue
-
-
-                thread = Thread(target=self._r_sort, args=(path,))
+                thread = Thread(target=self._scan_folder, args=(path,))
             else:
                 thread = Thread(target=self._move_file, args=(path,))
-
 
             thread.start()
             self.threads.append(thread)
 
     def _move_file(self, path_file: Path):
-        path_destination_folder: Path
-        new_name_file: str
-        new_path_file: Path
-
+        """The function of moving a file"""
         logging.debug(f'Moving file: {path_file.name}')
 
-        # Визначення кінцевої папки
+        name_file = path_file.stem
+        file_suffix = path_file.suffix
+        # Define the destination folder
+        type_file = (
+            self._define_destination_folder(path_file))
+
+        # Normalize file name
+        if not type_file == "other":
+            name_file = self.normalize_name(name_file)
+
+        # Check for duplicates
+        name_file = self._check_duplicates(
+            path_file,
+            type_file)
+
+        if type_file == "archives":
+            try:
+                destination_folder = self._create_folder(type_file)
+
+                shutil.unpack_archive(
+                    path_file,
+                    Path(destination_folder, name_file))
+                path_file.unlink()
+            except:
+                destination_folder = self._create_folder("other")
+                shutil.move(
+                    path_file,
+                    Path(destination_folder, path_file.name))
+        else:
+            destination_folder = self._create_folder(type_file)
+
+            final_path_file = Path(
+                destination_folder,
+                f"{name_file + file_suffix}")
+
+            shutil.move(path_file, final_path_file)
+
+    def _create_folder(self, name_folder) -> Path:
+        """Function creates folder
+        if it does not exist"""
+        destination_folder = Path(
+            self.start_folder,
+            name_folder)
+
+        if not destination_folder.exists():
+            destination_folder.mkdir()
+
+        return destination_folder
+
+    def _define_destination_folder(self, path_file: Path) -> str:
+        """The function determines the final
+        folder for the file"""
         for key, value in self.TYPE_FILE.items():
             if path_file.suffix.lstrip(".").upper() in value:
-                path_destination_folder = Path(self.start_folder, key)
-                break
+                return key
         else:
-            path_destination_folder = Path(self.start_folder, 'other')
-        
-        # Створення кінцевої папки, якщо не існує
-        if not path_destination_folder.exists():
-            path_destination_folder.mkdir()
+            return "other"
 
-        
-
-        # Нормалізація імені файла
-        if not path_destination_folder.name == "other":
-            new_name_file = self.normalize_name(path_file.stem)
-        else:
-            new_name_file = path_file.stem
-
-        # Перевірка дублікатів
+    def _check_duplicates(self,
+                          path_file: Path,
+                          type_file: Path):
+        """The function checks whether files with
+        this name exist in the target folder.
+        If such a file already exists, a suffix
+        is added to the file name."""
         count = 0
 
         while True:
-            name = new_name_file
+            name = path_file.stem
             new_path: Path
-            
-            if count:
-                name = f"{new_name_file}_{count}"
 
-            new_path = Path(path_destination_folder, f"{name}{path_file.suffix}")
-            
+            if count:
+                name = f"{name}_{count}"
+
+            new_path = Path(
+                self.start_folder,
+                type_file,
+                f"{name}{path_file.suffix}")
+
             if new_path.exists():
                 count += 1
                 continue
-            
-            new_name_file = name
-            break
-        
-        new_path_file = Path(path_destination_folder, f"{new_name_file}{path_file.suffix}")
 
-        if path_destination_folder.name == "archives":
-            try:
-                shutil.unpack_archive(path_file, Path(path_destination_folder, new_name_file))
-                path_file.unlink()
-            except:
-                shutil.move(path_file, Path(path_destination_folder.parent, "other", path_file.name))
-        else:
-            shutil.move(path_file, new_path_file)
-
+            return name
 
     def normalize_name(self, last_name):
-        """Normalize the input text by replacing non-alphanumeric characters with underscores
-           and applying Cyrillic to Latin transliteration."""
+        """Normalize the input text by replacing
+        non-alphanumeric characters with underscores
+        and applying Cyrillic to Latin transliteration."""
+
         # Initialize an empty string to store the normalized text
         new_name = ""
         # Iterate through each character in the input text
         for char in last_name:
-            # Replace non-alphanumeric characters with underscores, keep alphanumeric characters unchanged
+            # Replace non-alphanumeric characters
+            # with underscores, keep alphanumeric
+            # characters unchanged
             if not char.isalnum():
                 new_name += "_"
             else:
                 new_name += char
-        # Use the translate method to apply additional character mapping for transliteration
+        # Use the translate method to apply additional
+        # character mapping for transliteration
         return new_name.translate(self.MAP)
 
     def _clean_folder(self):
+        """The function clears the folder"""
         for path in self.start_folder.iterdir():
-            if not path.name in self.TYPE_FILE:
+            if path.name not in self.TYPE_FILE:
                 if path.is_dir():
                     shutil.rmtree(path)
                 else:
@@ -178,7 +218,9 @@ class FileOrganizer:
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(threadName)s %(message)s')
 
     thread = FileOrganizer(Path("/Users/nuambevos/Downloads/folder"))
     thread.start()
